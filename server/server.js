2643775f8a80253a7d99f744b1e0ef78697aee4b
Andrew Park <andrewsmacbookair@Andrews-MacBook-Air.local>
2022-09-14 17:21:21 -0700

    updated port

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors(corsOptions));
 const vendingRouter = require('./routes/vendingRouter.js');
 
 // SERVER WILL LISTEN ON PORT 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
